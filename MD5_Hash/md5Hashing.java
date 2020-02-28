af61f5d5b8d90ea8fafbd4a6ca147ce242e1dcf1
Im Rafid <36785493+WolfDroid@users.noreply.github.com>
2020-02-28 16:26:24 +0700

    Initial commit

---
+++ MD5_Hash/md5Hashing.java
@@ -0,0 +1,19 @@
+/**
+ * Class for hashing MD5
+ *
+ * @Ilham Mulya Rafid
+ * @version 28/02/2020
+ */
+//External Library for Hashing
+import java.math.BigInteger;                    //Math Library for Converting into BigInteger
+import java.security.MessageDigest;             //Security Library for Message Digest Hashing
+import java.security.NoSuchAlgorithmException;  //Securil
+
+//Hashing Class
+public class md5Hashing{
+    //Accessor Class
+    public static String getMd5(String input) {
+        
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cryptography
+ Repository for learning Cryptography
